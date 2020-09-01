6640d36407acf69ecd6cea922c79042e8b80330a
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-09-01 18:46:24 +0200

    Ajout test selenium

--- pur_beurre/product/tests/test_ux_product.py
+++ pur_beurre/product/tests/test_ux_product.py
@@ -1,21 +1,49 @@
-#import pytest
-#import os
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#os.environ["PATH"] += os.pathsep + os.path.join(BASE_DIR,'/driver/geckodriver')
+import pytest
+import os
+import pprint
+BASE_DIR = \
+    os.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.sep)[:-2])
+os.environ["PATH"] += os.pathsep + os.path.join(BASE_DIR,'driver')
 
-#from django.test import LiveServerTestCase
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-#class AccountTestCase(LiveServerTestCase):
-#    @pytest.mark.skipif('DEPLOY_ENVIRON' in os.environ and os.environ['DEPLOY_ENVIRON'] == 'PRODUCTION')
-#    def test_login(self):
-#        driver = webdriver.Firefox()
-#        driver.get('http://127.0.0.1:8000/accounts/login/')
-##        username = driver.find_element_by_id('id_username')
-##        password = driver.find_element_by_id('id_password')
-##        submit = driver.find_element_by_tag_name('button')
-##        username.send_keys('superusername')
-##        password.send_keys('superuserpassword')
-##        submit.send_keys(Keys.RETURN)
+
+@pytest.mark.skipif('DEPLOY_ENVIRON' in os.environ and os.environ['DEPLOY_ENVIRON'] == 'PRODUCTION',
+                    reason="requires production environement")
+class AccountTestCase(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+
+    def test_login(self):
+        self.driver.implicitly_wait(5)
+        self.driver.get('http://localhost:8000/user/login/')
+        username = self.driver.find_element_by_id('id_username')
+        username.send_keys('test_username')
+        self.driver.implicitly_wait(5)
+
+        password = self.driver.find_element_by_id('id_password')
+        password.send_keys('test_password')
+        self.driver.implicitly_wait(5)
+
+        try:
+            submit = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((((By.XPATH,'.//button[@type="submit" and text()="Login"]')))))
+            submit.click()
+        finally:
+            self.driver.quit()
+
 
