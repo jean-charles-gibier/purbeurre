66c979fe57df0f803789386ab16760506b32dc78
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-10-15 23:42:10 +0200

    Create deploy_ci.py

---
+++ pur_beurre/pur_beurre/settings/deploy_ci.py
@@ -0,0 +1,18 @@
+
+# CI : sim bdd de prod
+if 'DEPLOY_ENVIRON' in os.environ and os.environ['DEPLOY_ENVIRON'] == 'PRODUCTION':
+    DATABASES = {
+    
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'purbeurre',
+            'PASSWORD': 'purbeurre',
+            'HOST': 'purbeurre.ctquseoiqna8.eu-west-3.rds.amazonaws.com',
+            'PORT': 5432,
+            'TEST': {
+                        'NAME': 'test_postgres2',
+                    },
+        }
+    
+    }
